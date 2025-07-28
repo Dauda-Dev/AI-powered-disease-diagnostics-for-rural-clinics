3452dbb45cd23c981b0e533896fee84198209918
dauda.dauda <dauda933@gmail.com>
2025-07-28 13:10:47 +0100

    added update

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your-secret-key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', "postgresql://myuser:mypassword@localhost:5433/ai_clinic_db")
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', "postgresql://myuser:mypassword@localhost:5432/ai_clinic_db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET = os.getenv('JWT_SECRET', 'your-jwt-secret')
     GROQ_API_KEY = os.getenv('GROQ_API_KEY', '')
